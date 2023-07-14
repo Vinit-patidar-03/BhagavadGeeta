3bfb79f4c4568abe6e3596336426b60e8544df2d
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-07-14 16:52:51 +0530

    changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import {useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const Navigate = useNavigate();
+
   return (
     <>
         <nav className='bg-slate-200 fixed top-0 z-10 w-full'>
             <ul className='list-none'>
-                <li><img src="/images/BhagvadGita.png" className=' w-28' alt="logo" /></li>
+                <li onClick={()=>{Navigate('/')}} className='cursor-pointer'><img src="/images/BhagvadGita.png" className=' w-28' alt="logo" /></li>
             </ul>
         </nav>
     </>
