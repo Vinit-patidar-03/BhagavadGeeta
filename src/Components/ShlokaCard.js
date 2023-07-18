86403d6df707ae09efdc6ef0df6cdc66b090f704
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-07-18 13:01:23 +0530

    changes

--- src/Components/ShlokaCard.js
+++ src/Components/ShlokaCard.js
@@ -18,15 +18,17 @@ const ShlokaCard = (props) => {
         <>
             <div>
                 <div className='p-2 bg-slate-100'>
-                    <h1 className='font-bold'>Shloka: {Shloka.verse_number}</h1>
+                    <div className='flex items-center'>
+                        <h1 className='font-bold'>Shloka: {Shloka.verse_number}</h1>
+                        <i className="fa-solid fa-volume-high cursor-pointer mx-2" onClick={() => { readShloka(Shloka.text) }} title='click to speak'></i>
+                    </div>
                     <div className='flex justify-center flex-col mt-2'>
                         <h1 className='text-center font-semibold'>{Shloka.text.split('\n\n').length === 3 ? Shloka.text.split('\n\n').filter((elem, index) => { return index === 0 }) : ''}</h1>
-                            <div className='text-center p-3'>
-                                {
-                                    Shloka.text.split('\n\n').length === 3 ? Shloka.text.split('\n\n').filter((elem, index) => { return index !== 0 }).join('\n') : Shloka.text.split('\n\n').join('\n')
-                                }
-                                <i className="fa-solid fa-volume-high cursor-pointer" onClick={readShloka(Shloka.text)}></i>
-                            </div>
+                        <div className='text-center p-3'>
+                            {
+                                Shloka.text.split('\n\n').length === 3 ? Shloka.text.split('\n\n').filter((elem, index) => { return index !== 0 }).join('\n') : Shloka.text.split('\n\n').join('\n')
+                            }
+                        </div>
                     </div>
 
                 </div>
